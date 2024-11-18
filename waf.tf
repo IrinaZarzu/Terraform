98632ebba2ecf3bb5bf3293c52c9e4ed15e1937b
CloudInfra <116802082+IrinaZarzu@users.noreply.github.com>
2024-11-18 12:41:43 +0100

    Update waf.tf

--- waf.tf
+++ waf.tf
@@ -61,6 +61,28 @@ resource "aws_wafv2_web_acl" "alb_web_acl" {
     allow {}
   }
 
+rule {
+    name     = "AWSManagedRulesCommonRuleSet"
+    priority = 0
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet" 
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "CRSRule"
+      sampled_requests_enabled   = true
+    }
+  }
+
 
     visibility_config {
       cloudwatch_metrics_enabled = true
